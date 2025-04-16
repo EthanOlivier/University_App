a4e1ac7a3d34147b495ea72efe8beadb16bb92a0
Ethan <ethanolivieri56@gmail.com>
2025-04-16 12:03:34 -0400

    Added Student table and model

---
+++ app/models/student.rb
@@ -0,0 +1,7 @@
+class Student < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 5, maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i  
+  validates :email, presence: true, length: { maximum: 100 },            
+                    uniqueness: { case_sensitive: false },            
+                    format: { with: VALID_EMAIL_REGEX }
+end
