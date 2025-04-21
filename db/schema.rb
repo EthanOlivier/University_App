08e7806c5f4a4d1ed16f823ac9fa2620759195f4
Ethan <ethanolivieri56@gmail.com>
2025-04-21 13:06:55 -0400

    Added Password to Student model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_04_16_155630) do
+ActiveRecord::Schema.define(version: 2025_04_21_165754) do
 
   create_table "courses", force: :cascade do |t|
     t.string "short_name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2025_04_16_155630) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
