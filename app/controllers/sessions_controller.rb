e02fa8db13af018625b54735d42c4f78feb44945
Ethan <ethanolivieri56@gmail.com>
2025-04-21 14:46:11 -0400

    Added logging functionality with navigation through navbar

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,20 @@ class SessionsController < ApplicationController
   end
   
   def create
+    student = Student.find_by(email: params[:session][:email].downcase)
+    if student && student.authenticate(params[:session][:password])
+      session[:student_id] = student.id
+      flash[:notice] = "Logged in successfully"
+      redirect_to student
+    else
+      flash.now[:alert] = "Failed to log in. Email or password were incorrect"
+      render "new"
+    end
   end
 
   def destroy
+    session[:student_id] = nil
+    flash[:notice] = "Logged out successfully"
+    redirect_to root_path
   end
 end
