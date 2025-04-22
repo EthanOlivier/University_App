472c9e713151bea09885a27e779e7473649bf4e8
Ethan <ethanolivieri56@gmail.com>
2025-04-22 13:01:19 -0400

    1. Fixed bug where index pages would not be in uniformed columns if the cards were not the same length 2. Added Enrollment Functionallity 3. Added Profile Link in Navbar when logged in 4. Added Courses Enrollments to Students

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "courses#index"
   get "about", to: "pages#about"
   get "courses/new", to: "courses#new"
+  post "course_enroll", to: "student_courses#create"
   resources :students, except: [:destroy]
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
