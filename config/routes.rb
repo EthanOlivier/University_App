496c0dd956393b476c6f555b588c64ca646cee4a
Ethan <ethanolivieri56@gmail.com>
2025-04-22 13:19:37 -0400

    Added Courses show page and links between students and courses

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "courses#index"
   get "about", to: "pages#about"
   get "courses/new", to: "courses#new"
+  get "courses/:id", to: "courses#show", as: :course
   post "course_enroll", to: "student_courses#create"
   resources :students, except: [:destroy]
   get "login", to: "sessions#new"
